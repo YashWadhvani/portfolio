0ef54ff438cd9bbda632cea86e10d4241941212d
YashWadhvani <yashwadhvani123@gmail.com>
2023-12-14 02:11:22 +0530

    Initialised React App and Worked on Creating and Designing a Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Container, Navbar, Nav, Image } from "react-bootstrap";
+import incognito from "../assets/hacker.png";
+
+export default function NavBar() {
+  return (
+    <>
+      <Container className="navContainer">
+        <Navbar expand="lg" className="darkBg navbar-dark mx-2">
+          <Container className="darkBgBranding mx-auto">
+            <Image src={incognito} className="brandImg d-inline" />
+            <Navbar.Brand href="#home" className="darkBgText">
+              <p className="d-inline brandText">&lt; Yash Wadhvani &gt;</p>
+            </Navbar.Brand>
+          </Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link
+                href="#home"
+                className="darkBgText hover-underline-animation"
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#about"
+                className="darkBgText hover-underline-animation"
+              >
+                About Me
+              </Nav.Link>
+              <Nav.Link
+                href="#projects"
+                className="darkBgText hover-underline-animation"
+              >
+                Projects
+              </Nav.Link>
+              <Nav.Link
+                href="#contact"
+                className="darkBgText hover-underline-animation"
+              >
+                Contact Me
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+    </>
+  );
+}
