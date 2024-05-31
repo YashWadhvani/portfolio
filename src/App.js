c9e2c92d77cb4251d5e50cbc5d83f886dce360e1
YashWadhvani <yashwadhvani123@gmail.com>
2024-05-31 21:09:17 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-// import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import FeaturedProjects from "./components/FeaturedProjects";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-// import { Container } from "react-bootstrap";
 import NavBar from "./components/Navbar";
+import Footer from "./components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
       <SpeedInsights />
       <NavBar />
-      <Hero />
-      <FeaturedProjects />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/projects" element={<Projects />} />
+        <Route exact path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
     </>
   );
